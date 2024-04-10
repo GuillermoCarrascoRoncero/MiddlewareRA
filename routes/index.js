0ff228f49fce1d41083d94ce05622d65c9191cc7
GuillermoCarrascoRoncero <guillermo.carrasco.roncero@alumnos.upm.es>
2024-04-10 19:59:31 +0200

    CrateDB IMP

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var mqtt = require('mqtt');
+var crate = require('node-crate');
 
 const mqttClient = mqtt.connect('mqtt://localhost'); 
+
+crate.connect('localhost', 4200);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,33 +16,50 @@ router.get('/', function(req, res, next) {
 router.get('/record', function(req, res, next) {
 	var now = new Date();
 var logfile_name = __dirname+'/../public/logs/' +req.query.id_nodo+ "-"+ now.getFullYear() + "-"+ now.getMonth() + "-" + now.getDate() +'.csv'
-mqttClient.publish('sensor-topic', JSON.stringify(req.query));
 
-fs.stat(logfile_name, function(err, stat) {
-    if(err == null) {
-        console.log('File %s exists', logfile_name);
-		let content = req.query.id_nodo+';'+now.getTime()+";"+req.query.temperatura+";"+req.query.humedad+";"+req.query.co2+";"+req.query.volatiles+"\r\n";
-		append2file(logfile_name, content);
-		
-    } else if(err.code === 'ENOENT') {
-        // file does not exist
-		let content ='id_nodo; timestamp; temperatura; humedad; CO2; volatiles\r\n'+req.query.id_nodo+';'+now.getTime()+";"+req.query.temperatura+";"+req.query.humedad+";"+req.query.co2+";"+req.query.volatiles+"\r\n";
-       append2file(logfile_name, content);
-    } else {
-        console.log('Some other error: ', err.code);
-    }
-});
+  const data = req.query;
+  
+  crate.execute("INSERT INTO ra_table (id_nodo, time, temperatura, humedad, co2, volatiles) VALUES (?, ?, ?, ?, ?, ?)", [data.id_nodo, now.getTime(), data.temperatura, data.humedad, data.co2, data.volatiles])
+    .then((result) => {
+      console.log('Datos enviados a la DB:', result);
+    })
+    .catch((error) => {
+      console.error('Error al enviar datos:', error);
+    });
+	
+	mqttClient.publish('sensor-topic', JSON.stringify(req.query));
 
+	fs.stat(logfile_name, function(err, stat) {
+		if(err == null) {
+			console.log('File %s exists', logfile_name);
+			let content = req.query.id_nodo+';'+now.getTime()+";"+req.query.temperatura+";"+req.query.humedad+";"+req.query.co2+";"+req.query.volatiles+"\r\n";
+			append2file(logfile_name, content);
+			
+		} else if(err.code === 'ENOENT') {
+			// file does not exist
+			let content ='id_nodo; timestamp; temperatura; humedad; CO2; volatiles\r\n'+req.query.id_nodo+';'+now.getTime()+";"+req.query.temperatura+";"+req.query.humedad+";"+req.query.co2+";"+req.query.volatiles+"\r\n";
+		   append2file(logfile_name, content);
+		} else {
+			console.log('Some other error: ', err.code);
+		}
+	});
 
-
-
-  //res.render('index', { title: 'Express' });
-  res.send("Saving: "+req.query.id_nodo+';'+now.getTime()+";"+req.query.temperatura+";"+req.query.humedad+";"+req.query.co2+";"+req.query.volatiles+" in: "+logfile_name);
+   //res.render('index', { title: 'Express' });
+   res.send("Saving: "+req.query.id_nodo+';'+now.getTime()+";"+req.query.temperatura+";"+req.query.humedad+";"+req.query.co2+";"+req.query.volatiles+" in: "+logfile_name);
 });
 
 // POST handler
 router.post('/record', function(req, res, next) {
   const data = req.body;
+  
+  crate.execute("INSERT INTO ra_table (id_nodo, time, temperatura, humedad, co2, volatiles) VALUES (?, ?, ?, ?, ?, ?)", [data.id_nodo, fechaLegible, data.temperatura, data.humedad, data.co2, data.volatiles])
+    .then((result) => {
+      console.log('Datos enviados a la DB:', result);
+    })
+    .catch((error) => {
+      console.error('Error al enviar datos:', error);
+    });
+	
   mqttClient.publish('sensor-topic', JSON.stringify(data));
   res.sendStatus(200);
 });
